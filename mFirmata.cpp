d46a9352b7793f7eaaf47dfde084591a8c7427c3
gastonfeng <gastonfeng@gmail.com>
2023-06-19 18:25:59 +0800

    kt1260a_rte_2.2.956_2023-06-19

--- mFirmata.cpp
+++ mFirmata.cpp
@@ -138,7 +138,7 @@ void mFirmata::reportAnalogCallback(nStream *stream, byte analogPin, int value) 
             bitSet(plc_var.info.analogInputsToReport, analogPin);
             // prevent during system reset or all analog pin values will be reported
             // which may report noise for unconnected analog pins
-            if (plc_var.info.state < pb_app_state_APP_FLASH_BEGIN) {
+            if (plc_var.info.state < pb_app_state_FLASH_FORMAT) {
                 // Send pin value immediately. This is helpful when connected via
                 // ethernet, wi-fi or bluetooth so pin states can be known upon
                 // reconnecting.
